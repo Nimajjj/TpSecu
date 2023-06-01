263f816914cece5f385efb6be9c6316716b8dc70
Nimajjj <benjamin.borello@aol.com>
2023-06-01 23:28:53 +0200

    chore(format): fix inconsistant tab formating

--- dal/query.php
+++ dal/query.php
@@ -1,26 +1,26 @@
 <?php
 
 class Query {
-    public $table;
-    public $conditions = [];
-    public $parameters = [];
-    
-    public function __construct($_table) {
-        $this->table = $_table; 
-        return $this;
-    }
+  public $table;
+  public $conditions = [];
+  public $parameters = [];
+  
+  public function __construct($_table) {
+    $this->table = $_table; 
+    return $this;
+  }
 
-    public function Condition($_column, $_condition, $_value) {
-        $this->conditions = [
-            'column' => $_column,
-            'condition' => $_condition, 
-            'value' => $_value
-        ];
-        return $this;     
-    }
+  public function Condition($_column, $_condition, $_value) {
+    $this->conditions = [
+        'column' => $_column,
+        'condition' => $_condition, 
+        'value' => $_value
+    ];
+    return $this;     
+  }
 
-    public function Parameter($_parameters) {
-      $this->parameters = $_parameters;
-      return $this;
-    }
+  public function Parameter($_parameters) {
+    $this->parameters = $_parameters;
+    return $this;
+  }
 }
