872a4581338652c2e9df6945ad7e8273554c3d66
Nimajjj <benjamin.borello@aol.com>
2023-06-09 15:56:23 +0200

    feat(env): add .env file which store db access and token secret

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 require_once("vendor/autoload.php");
 
 use App\Router\Router;
+use App\Controller\Authorizer\Authorizer;
 
+// load .env file
+Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
+// handle routes
 new Router();
+
+var_dump(Authorizer::GetSessionState());
